b5973d9d1f5268b03707f121492d97387498e4f5
Mike <miguelito.guerra@hotmail.com>
2026-03-23 08:34:44 -0600

    Tabs arreglados

--- src/main.py
+++ src/main.py
@@ -2,8 +2,8 @@ import utils
 
 
 if __name__ == "__main__":
-    print("Iniciando programa principal")
-    utils.barra_carga(2, "Cargando")
-    print("\n")
-    utils.saludo()
+	print("Iniciando programa principal")
+	utils.barra_carga(2, "Cargando")
+	print("\n")
+	utils.saludo()
 	print("Test, prueba de GitHub y GitHub desktop")
