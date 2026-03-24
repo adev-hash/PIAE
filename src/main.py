806e85867ed00bbc6351118030943ded5df5737b
Mike <miguelito.guerra@hotmail.com>
2026-03-24 08:17:06 -0600

    Importación de códigos y manejo de errores

--- src/main.py
+++ src/main.py
@@ -1,9 +1,12 @@
-import utils
+import utils, api_cliente, limpiador_datos, analizador, graficas
 
 
 if __name__ == "__main__":
-	print("Iniciando programa principal")
-	utils.barra_carga(2, "Cargando")
-	print("\n")
-	utils.saludo()
-	print("Test, prueba de GitHub y GitHub desktop")
+	try:
+		print("Iniciando programa principal")
+		utils.barra_carga(2, "Cargando")
+		print("\n")
+		utils.saludo()
+		#print("Test, prueba de GitHub y GitHub desktop")
+	except ImportError:
+		print("Error: Módulos no encontrados")
