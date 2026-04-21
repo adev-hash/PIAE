c50f84d617659d99f2581de13a6562189e60bf30
Mike <miguelito.guerra@hotmail.com>
2026-04-20 20:45:42 -0600

    Agregada funcionalidad con api_cliente
    Utiliza el script de "api_cliente"
    
    Muestra la temperatura, humedad, descripcion del clima y medida del viento de una ciudad escrita.
    
    Se corrigió parte del código de api_cliente

--- src/main.py
+++ src/main.py
@@ -1,13 +1,36 @@
 import utils, api_cliente, limpiador_datos, analizador, graficas
 
+try:
+	print("Iniciando programa principal")
+	utils.barra_carga(2, "Cargando")
+	print("\n")
+	utils.saludo()
 
-if __name__ == "__main__":
-	try:
-		print("Iniciando programa principal")
-		utils.barra_carga(2, "Cargando")
-		print("\n")
-		utils.saludo()
-		#print("Test, prueba de GitHub y GitHub desktop")
-		print("Hola, así funciona GitHub")
-	except ImportError:
-		print("Error: Módulos no encontrados")
+except ImportError:
+	print("Error: Módulo utils no encontrado")
+
+
+#Obtener información de una API publica
+
+ciudad = input("Ingresa la ciudad a analizar: ")
+
+try:
+	datos = api_cliente.dato_clima(ciudad)
+except ImportError:
+	print("Módulo api_cliente no encontrado")
+
+datos = api_cliente.obtener_temperatura(ciudad)
+
+print(f"Temperatura: {datos}")
+
+datos = api_cliente.obtener_humedad(ciudad)
+
+print(f"Humedad: {datos}")
+
+datos = api_cliente.obtener_descripcion(ciudad)
+
+print(f"Descripción: {datos}")
+
+datos = api_cliente.obtener_viento(ciudad)
+
+print(f"Viento: {datos}")
